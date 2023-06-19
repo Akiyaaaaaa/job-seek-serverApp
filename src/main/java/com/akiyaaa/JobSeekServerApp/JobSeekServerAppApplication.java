d75e84b98462152125f6d70e9d33d367179e35b9
Zadid Taqwa <Zadidtaqwa123@gmail.com>
2023-06-19 14:27:57 +0700

    initialize job-seek server

---
+++ src/main/java/com/akiyaaa/JobSeekServerApp/JobSeekServerAppApplication.java
@@ -0,0 +1,13 @@
+package com.akiyaaa.JobSeekServerApp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JobSeekServerAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JobSeekServerAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/akiyaaa/JobSeekServerApp/JobSeekServerAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.akiyaaa.JobSeekServerApp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JobSeekServerAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
